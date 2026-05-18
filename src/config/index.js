869ba65eeb1fe88793f1120d7fb83a8995080675
olateju289 <olateju289@gmail.com>
2026-05-18 14:02:53 +0100

    Add keep-alive server

--- src/config/index.js
+++ src/config/index.js
@@ -27,4 +27,12 @@ module.exports = {
   cron: {
     schedule: process.env.CRON_SCHEDULE || '0 * * * *',
   },
+
+  const http = require('http');
+
+http.createServer((req, res) => {
+  res.write('Bot is running');
+  res.end();
+}).listen(process.env.PORT || 3001, () => {
+  console.log('Dashboard server running');
 };
